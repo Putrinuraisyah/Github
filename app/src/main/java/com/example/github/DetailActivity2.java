3fb73625a79ec1a50aae8ecc8f6a614eef60ff31
SaidahSamiyah <sayyidahsamiyah@gmail.com>
2021-10-23 12:56:28 +0700

    Singkronisasi Data lama

---
+++ app/src/main/java/com/example/github/DetailActivity2.java
@@ -0,0 +1,13 @@
+package com.example.github;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class DetailActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail2);
+    }
+}
